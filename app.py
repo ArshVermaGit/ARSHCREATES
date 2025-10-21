d28131963f4c90c21e5dfa011d14f54e9222609f
Arsh Verma <arshverma.dev@gmail.com>
2025-10-21 23:01:45 +0530

    Adding Elements for Railway.

--- app.py
+++ app.py
@@ -325,4 +325,4 @@ def lower_filter(s):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
