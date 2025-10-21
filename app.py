6cee5ad42a84ff105fd7c50a29ae2217f7187d4d
Arsh Verma <arshverma.dev@gmail.com>
2025-10-21 22:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -324,4 +324,5 @@ def lower_filter(s):
     return str(s).lower() if s else ''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
