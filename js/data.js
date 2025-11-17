033042b9085358502b41685aa25943adaa06725d
Arsh Verma <arshverma.dev@gmail.com>
2025-11-18 03:03:26 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -80,7 +80,7 @@ const PORTFOLIO_DATA = {
                 "Privacy-focused design"
             ],
             repositoryUrl: "https://github.com/ArshVermaGit/REELSPOT",
-            liveUrl: "https://arshvermagit.github.io/REELSPOT/",
+            liveUrl: "https://arshvermagit-reelspot.netlify.app",
             screenshots: [
                 "static/images/websites/ReelSpot/1.jpg",
                 "static/images/websites/ReelSpot/2.jpg",
